2672f61280ea32339e10e16653fa0f99c3cc3ca6
a1k0u <alko7enko@gmail.com>
2022-08-24 01:57:02 +0300

    regex fix

--- config.py
+++ config.py
@@ -3,3 +3,6 @@ import os
 api_url = os.getenv("TELEGRAM_URL")
 api_token = os.getenv("TELEGRAM_TOKEN")
 server_url = os.getenv("SERVER_URL")
+
+title_pattern = r"(?<=\<span\>)[а-яА-Я\w\!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\`\{\|\}\~\\\ ]*(?=\<\/span\>\<\/h1\>)"
+url_pattern = r"(?<=https\:\/\/habr.com\/)[\w\-\&\=\:\?\/\.]*(?=)"
--- method.py
+++ method.py
@@ -7,10 +7,7 @@ from typing import Tuple
 
 import requests
 
-from config import api_url
-from config import api_token
-from config import server_url
-
+import config as cfg
 
 Post = Tuple[str, str]
 Code = int
@@ -27,16 +24,13 @@ def get_posts(msg: dict) -> Tuple[Code, List[Post]]:
         end_pos = start_pos + entity["length"]
 
         if _type == "url":
-            url_pattern = r"(?<=https\:\/\/habr.com\/)[\w\-\&\=\:\?\/\.]*(?=)"
             url = text[start_pos:end_pos]
 
-            if not re.findall(url_pattern, url):
+            if not re.findall(cfg.url_pattern, url):
                 continue
 
-            title_pattern = r"(?<=<h1\ data\-test\-id\=\"articleTitle\"\ class=\"tm\-article\-snippet\_\_title\ " \
-                            r"tm\-article\-snippet\_\_title\_h1\"\ lang\=\"ru\"\>\<span\>)[а-яА-Я\w\!\"\#\$\%\&\'\(" \
-                            r"\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\_\`\{\|\}\~\\\ ]*(?=\<\/span\>\<\/h1\>) "
-            title = re.findall(title_pattern, requests.get(url).content.decode("utf-8"))
+            page_content = requests.get(url).content.decode("utf-8")
+            title = re.findall(cfg.title_pattern, page_content)
 
             posts.append((title, url))
 
@@ -53,7 +47,7 @@ def get_posts(msg: dict) -> Tuple[Code, List[Post]]:
 
 
 def send_message(chat_id: int, text: str) -> int:
-    url: str = f"{api_url}{api_token}/sendMessage"
+    url: str = f"{cfg.api_url}{cfg.api_token}/sendMessage"
     data = {"chat_id": chat_id, "text": text}
 
     requests.post(url, data=data)
@@ -70,12 +64,12 @@ def delete_post(chat_id: str, post_id: str):
 
 
 def set_webhook() -> None:
-    url: str = f"{api_url}{api_token}/setWebhook"
-    response = requests.get(url, params={"url": server_url})
+    url: str = f"{cfg.api_url}{cfg.api_token}/setWebhook"
+    response = requests.get(url, params={"url": cfg.server_url})
     print(f"Set webhook: {response.status_code}")
 
 
 def delete_webhook() -> None:
-    url: str = f"{api_url}{api_token}/deleteWebhook"
+    url: str = f"{cfg.api_url}{cfg.api_token}/deleteWebhook"
     response = requests.get(url)
     print(f"Delete webhook: {response.status_code}")
