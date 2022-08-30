d5ea15266031f2344731105a1fcab14b9259e5e5
a1k0u <alko7enko@gmail.com>
2022-08-30 20:04:57 +0300

    add logger

--- config.py
+++ config.py
@@ -1,4 +1,14 @@
+import logging
 import os
+
+logging.basicConfig(
+    filename="logger.log",
+    format="%(asctime)s %(levelname)s in %(module)s with message: '%(message)s'",
+    level="NOTSET",
+)
+
+logger = logging.getLogger("habr")
+
 
 api_url = os.getenv("TELEGRAM_URL")
 api_token = os.getenv("TELEGRAM_TOKEN")
@@ -9,3 +19,16 @@ title_pattern = (
     r"?=\<\/span\>\<\/h1\>) "
 )
 url_pattern = r"(?<=https\:\/\/habr.com\/)[\w\-\&\=\:\?\/\.]*(?=)"
+
+if __name__ == "__main__":
+    print(
+        "".join(
+            [
+                f"{var} = {value}\n"
+                for var, value in locals().items()
+                if not var.startswith("__")
+            ]
+        )
+    )
+
+    logger.debug("Printed all local variables in config.py!")
