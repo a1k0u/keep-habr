d5da9fb7c0033597e8d58beaeefb29046dfad9ca
a1k0u <alko7enko@gmail.com>
2022-08-31 11:24:03 +0300

    change emoji to unicode

--- emoji.py
+++ emoji.py
@@ -1,16 +1,16 @@
 def get_emoji_from_int(number: int) -> str:
     """Returns list of emoji unicodes by number"""
     numbers = {
-        0: "0️⃣",
-        1: "1️⃣",
-        2: "2️⃣",
-        3: "3️⃣",
-        4: "4️⃣",
-        5: "5️⃣",
-        6: "6️⃣",
-        7: "7️⃣",
-        8: "8️⃣",
-        9: "9️⃣",
+        "0": u"\U00000030\U0000FE0F\U000020E3",
+        "1": u"\U00000031\U0000FE0F\U000020E3",
+        "2": u"\U00000032\U0000FE0F\U000020E3",
+        "3": u"\U00000033\U0000FE0F\U000020E3",
+        "4": u"\U00000034\U0000FE0F\U000020E3",
+        "5": u"\U00000035\U0000FE0F\U000020E3",
+        "6": u"\U00000036\U0000FE0F\U000020E3",
+        "7": u"\U00000037\U0000FE0F\U000020E3",
+        "8": u"\U00000038\U0000FE0F\U000020E3",
+        "9": u"\U00000039\U0000FE0F\U000020E3",
     }
 
-    return "".join([numbers[int(el)] for el in str(number)])
+    return "".join([numbers[el] for el in str(number)])
